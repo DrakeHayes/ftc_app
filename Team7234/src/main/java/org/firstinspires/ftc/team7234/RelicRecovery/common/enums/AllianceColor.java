4f21ea20df2b40f78cc23cd3a1a6364c1273bfc3
Donald Brown <donaldbrown2019@gmail.com>
2018-09-20 17:26:36 -0400

    # Rover Ruckus Preparation + Moved Last year's Botman Code to RelicRecovery Package + Created New RoverRuckus Package for 7234 + Updated build tool versions for new Android Studio Versions + Started new Rover Ruckus code base. + Added OpenCV

---
+++ Team7234/src/main/java/org/firstinspires/ftc/team7234/RelicRecovery/common/enums/AllianceColor.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.team7234.RelicRecovery.common.enums;
+
+public enum AllianceColor {
+    RED, BLUE
+}
--- Team7234/src/main/java/org/firstinspires/ftc/team7234/RoverRuckus/common/Hardware.java
+++ Team7234/src/main/java/org/firstinspires/ftc/team7234/RoverRuckus/common/Hardware.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.team7234.RoverRuckus.common;
 
+import android.util.Log;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.opencv.android.OpenCVLoader;
 
 public class Hardware {
 
@@ -31,18 +34,37 @@ public class Hardware {
         hwMap = ahwMap;
 
         //Define and initialize Motors
+        leftTank    = hwMap.get(DcMotor.class, "Left Tank");
+        leftRear    = hwMap.get(DcMotor.class, "Left Rear");
+        rightTank   = hwMap.get(DcMotor.class, "Right Tank");
+        rightRear   = hwMap.get(DcMotor.class, "Right Rear");
 
         //Set all motors to zero power
+        leftTank.setPower(0.0);
+        leftRear.setPower(0.0);
+        rightTank.setPower(0.0);
+        rightRear.setPower(0.0);
 
         //resets encoders
         resetEncoders();
         //Define and initialize servos
 
         //Define sensors
+
+        //Set up OpenCV
+        
     }
 
     public void resetEncoders(){
+        leftTank.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightTank.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        leftTank.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightTank.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
 }
