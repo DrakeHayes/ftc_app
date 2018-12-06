c7d4d60fdd5effa088979266348127e16370bf12
Donald Brown <donaldbrown2019@gmail.com>
2018-12-06 17:56:18 -0500

    Moves robot forward to detach from the latch.  Code added to CASE FORWARD.

--- Team7234/src/main/java/org/firstinspires/ftc/team7234/RoverRuckus/opmodes/Autonomous/RedDepot.java
+++ Team7234/src/main/java/org/firstinspires/ftc/team7234/RoverRuckus/opmodes/Autonomous/RedDepot.java
@@ -5,7 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.team7234.RoverRuckus.common.AutoBase;
 import org.firstinspires.ftc.team7234.RoverRuckus.common.enums.AllianceColor;
 import org.firstinspires.ftc.team7234.RoverRuckus.common.enums.FieldPosition;
-
+/*
+Because the field is split down the center, we can use the same code to perform some of the same
+tasks.  So, the BlueCrater, BlueDepot, RedCrater, and RedDepot opmodes all extend to the AutoBase
+opmode.
+ */
 @Autonomous(name = "Red Depot Autonomous", group = "Botman")
 public class RedDepot extends AutoBase {
     public RedDepot(){
